f5bc5f42c469030e56d099897b43aef19d20a380
MANTAGOUD <138131349+mantagouda@users.noreply.github.com>
2023-07-05 13:28:50 +0530

    first

---
+++ App.java
@@ -0,0 +1,23 @@
+package spring3.core3;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+// @primary
+//@component
+//@configuration
+//@autowired
+//@qualifier
+
+//annotation component autowired primary qualifier
+// her ewe will study that how to create beans with out using bean tag
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
